7c27254e600f7c3ea1a067eeb6e64050597ea77b
Manuelhp24 <manuel2412hm@gmail.com>
2025-08-08 23:31:45 -0500

    Update script.js
    Corrección de las primeras dos funciones agregándoles él ||

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // 1. Función declarativa - Consumo mensual (kWh)
 function consumoMensual() {
-  return parseFloat(prompt("Ingrese el consumo en kWh (ej: 410):"));
+  return parseFloat(prompt("Ingrese el consumo en kWh (ej: 410):")) || 410;
 }
 // 2. Función declarativa - Tarifa base por kWh
 function tarifaBase() {
-  return parseFloat(prompt("Ingrese la tarifa por kWh (ej: 620):"));
+  return parseFloat(prompt("Ingrese la tarifa por kWh (ej: 620):")) || 620;
 }
