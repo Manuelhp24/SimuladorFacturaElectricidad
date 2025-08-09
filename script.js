11334ede2d7c918a93d628c19802c52ba75673ff
Manuelhp24 <manuel2412hm@gmail.com>
2025-08-08 23:01:02 -0500

    Creación de función tarifaBase, función  (declarativa)

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 function consumoMensual() {
   return parseFloat(prompt("Ingrese el consumo en kWh (ej: 410):"));
 }
+// 2. Función declarativa - Tarifa base por kWh
+function tarifaBase() {
+  return parseFloat(prompt("Ingrese la tarifa por kWh (ej: 620):"));
+}
