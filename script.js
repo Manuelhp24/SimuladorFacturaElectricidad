0e2e5234ae3ad04eb6a8fa1289273342a3a56cbd
Manuelhp24 <manuel2412hm@gmail.com>
2025-08-08 23:54:49 -0500

    Creación de función cargoFijo, función de expreción

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // 1. Función declarativa - Consumo mensual (kWh)
 function consumoMensual() {
   return parseFloat(prompt("Ingrese el consumo en kWh (ej: 410):" || 410));
-};
+}
 // 2. Función declarativa - Tarifa base por kWh
 function tarifaBase() {
   return parseFloat(prompt("Ingrese la tarifa por kWh (ej: 620):")) || 620;
-};
+}
 // 3. Función de expresión - Cargo fijo
 const cargoFijo = function() {
   const cargoFijo = 3500;
